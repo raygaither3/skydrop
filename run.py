90647b715eaba823429920de1fb57db08d532bcd
raygaither3 <raygaither3@gmail.com>
2026-03-17 03:31:46 -0500

    Fix error on run.py

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
-git add requirements.txt
-git commit -m "Add geopy dependency"
-git push
+import os
+from app import create_app
+
+app = create_app()
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
